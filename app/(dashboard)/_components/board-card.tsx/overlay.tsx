ac487d0afc78e150b8c8129846474aa5b2896502
ramemes <17809655+ramemes@users.noreply.github.com>
2024-05-18 17:22:37 +0100

    rename board, delete board

--- app/(dashboard)/_components/board-card.tsx/overlay.tsx
+++ app/(dashboard)/_components/board-card.tsx/overlay.tsx
@@ -1,7 +1,7 @@
 export const Overlay = () => {
   return (
     <div
-      className="opacity-0 group-hover:opacity-10 transition-opacity
+      className="opacity-0 group-hover:opacity-40 transition-opacity
       h-full w-full bg-black"
     />
   )
